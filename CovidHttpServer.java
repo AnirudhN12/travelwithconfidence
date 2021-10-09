98dcf3b7b70a21bf41e053bbacc546a1730802f5
AnirudhN12 <92196427+AnirudhN12@users.noreply.github.com>
2021-10-09 14:27:03 +0530

    Add files via upload

--- CovidHttpServer.java
+++ CovidHttpServer.java
@@ -1,10 +1,13 @@
 package com.covid.server;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.List;
 
-import com.covid.basic.COVIDDataByState;
+import com.covid.basic.CaseData;
+import com.covid.basic.VaccinationData;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -23,20 +26,7 @@ public class CovidHttpServer {
 		try {
 			this.httpServer = HttpServer.create(new InetSocketAddress(8500), 10);
 			HttpContext context = this.httpServer.createContext("/twc");
-			context.setHandler(new HttpHandler() {
-				@Override
-				public void handle(HttpExchange exchange) throws IOException {
-					StringBuilder builder = new StringBuilder();
-					builder.append("Travel with confidence @ " + exchange.getRequestURI()).append("\n");
-					COVIDDataByState sd = new COVIDDataByState("Karnataka", 30);
-					builder.append(sd.getCaseData()).append("\n");
-					builder.append(sd.getVaccinationData()).append("\n");
-				    exchange.sendResponseHeaders(200, builder.toString().getBytes().length);
-				    OutputStream os = exchange.getResponseBody();
-				    os.write(builder.toString().getBytes());
-				    os.close();					
-				}
-			});
+			context.setHandler(new HttpRequestHandler());
 		} catch (IOException e) {
 			throw new RuntimeException("Error creating HTTP Server", e);
 		}
@@ -46,6 +36,39 @@ public class CovidHttpServer {
 		this.httpServer.start();
 	}
 	
+	private class HttpRequestHandler implements HttpHandler {
+
+		@Override
+		public void handle(HttpExchange exchange) throws IOException {
+			String state = null;
+			if(exchange.getRequestURI().toString().contains("?")) {
+				state =
+						exchange.getRequestURI().toString().
+							split("\\?")[1].
+							split("=")[1];
+				System.out.println(state);
+			}
+			try(FileInputStream fis = new FileInputStream("src/com/covid/server/website.html")) {
+			    exchange.sendResponseHeaders(200, 0);
+			    OutputStream os = exchange.getResponseBody();
+			    os.write(fis.readAllBytes());
+			    if(state != null) {
+			    	getCOVIDData(state, os);
+			    }
+			    os.close();
+			}
+		}
+		private void getCOVIDData(String state, OutputStream os) {
+			try {
+				List<CaseData> caseData = CaseData.initialiseCaseData(state);
+				List<VaccinationData> vaccinationData = VaccinationData.initialiseVaccinationData(state);
+				os.write(caseData.get(0).toString().getBytes());
+				os.write(vaccinationData.get(0).toString().getBytes());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		CovidHttpServer server = new CovidHttpServer();
@@ -53,3 +76,4 @@ public class CovidHttpServer {
 	}
 
 }
+
